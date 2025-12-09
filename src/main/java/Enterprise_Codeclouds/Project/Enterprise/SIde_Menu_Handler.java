de8dbba8e3aabefa399584d06fed56b0f8698ec1
AyanAutomation <sengupta.ayan90@gmail.com>
2025-12-09 19:18:20 +0530

    Started creating sidemenu module accessor

---
+++ src/main/java/Enterprise_Codeclouds/Project/Enterprise/SIde_Menu_Handler.java
@@ -0,0 +1,24 @@
+package Enterprise_Codeclouds.Project.Enterprise;
+
+import Locaters.Sidemenu_Locaters;
+
+public class SIde_Menu_Handler extends Base{
+	
+	
+    public void Side_menu_option_clicker(){
+    	
+    	Sidemenu_Locaters p = new Sidemenu_Locaters(d);
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }	
+	
+	
+
+
+}
---
+++ src/main/java/Locaters/Sidemenu_Locaters.java
@@ -0,0 +1,206 @@
+package Locaters;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Repeatative_codes.Repeat;
+
+public class Sidemenu_Locaters extends Repeat{
+
+
+	@FindBy(xpath="//*[@role='menu']")
+	private WebElement Side_Menu;/*
+	@FindBy(xpath="")
+	private WebElement  ; 
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ; */
+	
+	
+	public Sidemenu_Locaters(WebDriver d){
+	super(d);	
+	PageFactory.initElements(d, this);}
+	
+	
+	public WebElement Side_Menu(){
+	wait_for_theElement(Side_Menu);
+	return Side_Menu;}
+	public List <WebElement> Sidemenu_Options(){
+	Side_Menu();
+	List <WebElement> Sidemenu_Options = Side_Menu().findElements(By.xpath(".//*[@role='menuitem']"));
+	wait_for_theElement(Sidemenu_Options);
+	return Sidemenu_Options;}
+	public List <WebElement> Dropdown_submenu_options(){
+	Side_Menu();
+	List <WebElement> Dropdown_submenu_options = Side_Menu().findElements(By.xpath(".//li[@role='none']"));
+	wait_for_theElement(Dropdown_submenu_options);
+	return Dropdown_submenu_options;}/*
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}
+	public WebElement (){
+	wait_for_theElement();
+	return ;}  */
+	
+	
+}
