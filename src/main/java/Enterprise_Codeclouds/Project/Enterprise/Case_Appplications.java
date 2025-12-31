ae00ed8e88d75b924cf7158079e421354cb8fa69
AyanAutomation <sengupta.ayan90@gmail.com>
2025-12-31 13:51:24 +0530

    new plaintiff data

--- src/main/java/Enterprise_Codeclouds/Project/Enterprise/Case_Appplications.java
+++ src/main/java/Enterprise_Codeclouds/Project/Enterprise/Case_Appplications.java
@@ -1068,11 +1068,11 @@ public class Case_Appplications extends Header_Manager{
 		    }
 
 		    // ===== DataProvider return =====
-		    return new Object[][]{ /*
+		    return new Object[][]{ 
 		      {c1},{c2},{c3},{c4},{c5},
 		        {c6},{c7},{c8},{c9},{c10},
-		        {c11},{c12},{c13},{c14},{c15}, */
-		        {c16},/*{c17},{c18},{c19},{c20} */
+		        {c11},{c12},{c13},{c14},{c15}, 
+		        {c16},{c17},{c18},{c19},{c20} 
 		    };}
 	
 	
